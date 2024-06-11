28cf134f2b7c579d8095261f81b5246f1f9fa315
HA_Hun <96hunanyan96@gmail.com>
2024-06-11 21:01:25 +0400

    feature added react router and implement logic for that

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
-import './App.css'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
-function App() {
+const App: React.FC = () => {
+    const navigate = useNavigate()
+
+    const handleClick = async (
+        event: React.MouseEvent<HTMLAnchorElement>,
+        type: string,
+        path: string
+    ) => {
+        event.preventDefault()
+        console.log(path, 'path')
+        navigate(path)
+    }
+
     return (
-        <div className="App">
-            <div>asd</div>
+        <div>
+            <button id="trackButton">Click me</button>
+            <ul>
+                <li>
+                    <Link
+                        to="/1"
+                        onClick={e => handleClick(e, 'click-link', '1')}
+                    >
+                        1.html
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/2"
+                        onClick={e => handleClick(e, 'click-link', '2')}
+                    >
+                        2.html
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/3"
+                        onClick={e => handleClick(e, 'click-link', '3')}
+                    >
+                        3.html
+                    </Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/1" element={<div>Content for 1.html</div>} />
+                <Route path="/2" element={<div>Content for 2.html</div>} />
+                <Route path="/3" element={<div>Content for 3.html</div>} />
+            </Routes>
         </div>
     )
 }
