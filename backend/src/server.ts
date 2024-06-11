f90dac97c57eb3e9df69889e50faa1f79cab7a63
HA_Hun <96hunanyan96@gmail.com>
2024-06-11 18:21:25 +0400

    server setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-
 const app = express();
 const PORT = 8888;
 
 app.use(bodyParser.json());
 
-
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+    console.log(`http://localhost:${PORT}`);
 });
