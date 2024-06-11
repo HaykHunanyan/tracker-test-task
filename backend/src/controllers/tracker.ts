d04476c997c38fcf77858f54473c3a3b183777f8
HA_Hun <96hunanyan96@gmail.com>
2024-06-11 23:33:01 +0400

    finish

--- backend/src/controllers/tracker.ts
+++ backend/src/controllers/tracker.ts
@@ -2,6 +2,7 @@ import { validateEvents } from '../validate'
 import { TrackEvent } from '../models/TrackEvent'
 import path from 'path'
 import { Request, Response } from 'express'
+import { red } from 'colorette'
 
 export const Tracker = {
     AddEVENTS: async (req: Request, res: Response) => {
@@ -13,7 +14,7 @@ export const Tracker = {
             await TrackEvent.insertMany(events)
             res.status(200).send('OK')
         } catch (error) {
-            console.error('Error inserting events:', error)
+            console.error(red('Error inserting events:'), error)
             res.status(500).send('Internal Server Error')
         }
     },
